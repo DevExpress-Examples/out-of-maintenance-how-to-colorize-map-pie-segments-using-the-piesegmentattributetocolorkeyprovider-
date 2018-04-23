6fddced0fe4590bfe31bd0b4cfb8316ec6c5c654
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using DevExpress.XtraMap;
+
+namespace ColorizerPieSegmentAttributeProvider {
+    public partial class Form1 : Form {
+
+        public Form1() {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e) {
+            // Create a map control with initial settings and add it to the form.
+            MapControl map = new MapControl() { Dock = DockStyle.Fill };
+            this.Controls.Add(map);
+
+            // Create a layer to load image tiles from OpenStreetMap.
+            ImageTilesLayer tileLayer = new ImageTilesLayer();
+            tileLayer.DataProvider = new OpenStreetMapDataProvider();
+            map.Layers.Add(tileLayer);
+
+            // Create a vector items layer.
+            VectorItemsLayer itemsLayer = new VectorItemsLayer() {
+                Data = CreateData(),
+                Colorizer = CreateColorizer()
+            };
+            map.Layers.Add(itemsLayer);
+            
+            // Create a legend.
+            map.Legends.Add(new ColorListLegend() { Layer = itemsLayer });
+        }
+
+        private MapDataAdapterBase CreateData() {
+            MapItemStorage storage = new MapItemStorage();
+
+            MapPie pie = new MapPie() { Location = new GeoPoint(50, 13), Size = 100 };
+            pie.Segments.Add(CreatePieSegment("A", 100, "color1"));
+            pie.Segments.Add(CreatePieSegment("B", 50, "color2"));
+            pie.Segments.Add(CreatePieSegment("C", 120, "color3"));
+            storage.Items.Add(pie);
+
+            return storage;
+        }
+
+        private PieSegment CreatePieSegment(object argument, double value, object colorKey) {
+            PieSegment segment = new PieSegment();
+            segment.Argument = argument;
+            segment.Value = value;
+            segment.Attributes.Add(new MapItemAttribute() { Name = "color", Value = colorKey });
+            return segment;
+        }
+
+        private MapColorizer CreateColorizer() {
+            KeyValueColorizer colorizer = new KeyValueColorizer();
+            colorizer.AddItem("color1", new ColorizerColorTextItem() { Color = Color.Coral, Text = "Category A" });
+            colorizer.AddItem("color2", new ColorizerColorTextItem() { Color = Color.Orange, Text = "Category B" });
+            colorizer.AddItem("color3", new ColorizerColorTextItem() { Color = Color.LightBlue, Text = "Category C" });
+            
+
+            colorizer.ColorItemKeyProvider = new PieSegmentAttributeToColorKeyProvider() { AttributeName = "color" };
+            
+            return  colorizer;
+        }
+    }
+
+}
+
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to colorize map pie segments using the PieSegmentAttributeToColorKeyProvider class
+
+
+The following example shows how to colorize map pie segments using the <strong>PieSegmentAttributeToColorKeyProvider</strong> class as a color information provider for the colorizer.<br />For this, create a <strong>PieSegmentAttributeToColorKeyProvider</strong> object. Specify the <strong>PieSegmentAttributeToColorKeyProvider.AttributeName</strong> property value as a pie segment attribute name. Then assign the object to the <strong>KeyValueColorizer.ColorItemKeyProvider</strong> property. <br />Create a few <a href="https://documentation.devexpress.com/#WindowsForms/clsDevExpressXtraMapColorizerColorTextItemtopic">ColorizerColorTextItem</a> objects with keys, which  represent values of an attribute of pie segments, and add them to the colorizer via the <strong>KeyValueColorizer.AddItem</strong> method.
+
+<br/>
+
+
